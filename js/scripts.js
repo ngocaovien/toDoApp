61bc1665984e8c6c0f2324edb37f999309526ee9
ngocaovien <ngocaovien@gmail.com>
2018-06-25 18:37:30 -0700

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
-console.log("hello");
-// grab the to do Task
-let toDo = prompt("What you want to do?", "sleep");
-//create new li
-let newLi = document.createElement('li');
-const listOfToDo = document.querySelector(".to-do-list")
+//declaring variable for eventlistener.
+let submitBtn = document.querySelector('.todobtn');
+let deleteItem = document.querySelector('.delete');
+//creating the delete button
+let deleteBtn = document.createElement("button");
+let editBtn = document.createElement("button");
+let deleteText = document.createTextNode("Delete");
+let editText = document.createTextNode("Edit");
+deleteBtn.appendChild(deleteText);
+editBtn.appendChild(editText);
+deleteBtn.className ="delete";
+editBtn.className = "edit";
+//eventlistener below add items to the todo list.
+submitBtn.addEventListener('click', e =>{
+  let toDo = document.forms.todoform.todoitem.value;
+  let newLi = document.createElement('li');
+  newLi.className = "todoLi";
+  const listOfToDo = document.querySelector(".to-do-list");
+  newLi.textContent = toDo;
+  listOfToDo.appendChild(newLi);
+  let newLiWithBtn = newLi.appendChild(deleteBtn);
+  newLiWithBtn = newLi.appendChild(editBtn);
 
-//adding new todo to li.
+let deleteItem = document.querySelector('.delete');
+deleteItem.addEventListener('click' , e =>{
+  listOfToDo.removeChild(newLi);
 
-newLi.textContent = toDo;
-listOfToDo.appendChild(newLi);
+  let removedChild = listOfToDo.removeChild(newLi);
+  const listOfDone = document.querySelector(".done-list");
+  listOfDone.appendChild(removedChild);
+});
+
+  document.forms.todoform.reset();
+});
